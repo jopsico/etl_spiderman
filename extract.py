b25a3c81cfec2dac5b445f4912b15d3e942f2057
ALUNO C25 <alunoc25@unifor.local>
2025-10-28 20:06:20 -0300

    second

--- extract.py
+++ extract.py
@@ -1,5 +1,4 @@
 import requests
-import pandas as pd
 
 BASE_URL = "https://imdb.iamidiotareyoutoo.com/search?q=Spiderman"
 
@@ -7,24 +6,6 @@ def extract_data():
     response = requests.get(BASE_URL)
     response.raise_for_status()
     data = response.json()
+    print("JSON bruto extraído com sucesso!")
+    return data
 
-    filmes = data["description"]
-
-    df = pd.DataFrame([
-        {
-            "titulo": filme["#TITLE"],
-            "ano": filme["#YEAR"],
-            "atores": filme["#ACTORS"],
-            "id_imdb": filme["#IMDB_ID"],
-            "ranking": filme["#RANK"],
-            "aka": filme["#AKA"],
-            "url_imdb": filme["#IMDB_URL"],
-            "poster": filme["#IMG_POSTER"],
-            "largura_poster": filme["photo_width"],
-            "altura_poster": filme["photo_height"]
-        }
-        for filme in filmes
-    ])
-
-    print("Dados extraídos com sucesso!")
-    return df
